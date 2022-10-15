344f43373dcc2c6ac63cb3f7bff9b66063ed967d
youbinpark <you8inpark@gmail.com>
2022-10-16 01:50:47 +0900

    Modify: Login input 스타일 수정

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -67,29 +67,9 @@ const LoginForm = styled.form`
     display: flex;
     flex-direction: column;
 
-    .loginInput {
-      display: flex;
-      align-items: center;
-      margin-bottom: 10px;
-      padding: 10px;
-
-      border: 1px solid ${({ theme }) => theme.hyperMint};
-      border-radius: 0.625rem;
-
-      input {
-        width: 95%;
-        margin-left: 5px;
-
-        background: transparent;
-        border: none;
-        outline: none;
-      }
-    }
-
     button {
       margin: 10px 0;
       padding: 5px;
-
       background-color: transparent;
       border: 1px solid ${({ theme }) => theme.hyperMint};
     }
