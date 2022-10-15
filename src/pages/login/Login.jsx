eca5a6484d3b157a079a6d877a69c760f164d995
youbinpark <you8inpark@gmail.com>
2022-10-15 22:55:39 +0900

    Modify: Login 페이지 스타일 수정

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../../assets/images/logo1.png';
+import space from '../../assets/images/space.jpg';
 import Button from '../../components/button/Button';
 import { AiOutlineMail, AiFillLock } from 'react-icons/ai';
 import { FcGoogle } from 'react-icons/fc';
@@ -9,32 +10,46 @@ import styled from 'styled-components';
 
 const Login = () => {
   return (
-    <LoginForm>
-      <img src={logo} alt='logo' />
-      <p>
-        Don't have an account yet? <SignUp to='/signup'>Sign up</SignUp>
-      </p>
-      <div className='loginBox'>
-        <label>* Email </label>
-        <div className='loginInput'>
-          <AiOutlineMail />
-          <input type='email' placeholder='e-mail' />
-        </div>
-        <label>* Password </label>
-        <div className='loginInput'>
-          <AiFillLock />
-          <input type='password' placeholder='password' />
-        </div>
-        <Button type='submit' text='Sign In' />
-      </div>
-      <p> ↯ - or connect with - ↯ </p>
-      <div className='anotherLogin'>
-        <Button text={<FcGoogle />} />
-        <Button text={<SiKakaotalk className='kakao' />} />
-      </div>
-    </LoginForm>
+    <>
+      <img src={space} alt='background-image' />
+      <LoginPage>
+        <LoginForm>
+          <img src={logo} alt='logo' />
+          <p>
+            Don't have an account yet? <SignUp to='/signup'>Sign up</SignUp>
+          </p>
+          <div className='loginBox'>
+            <label>* Email </label>
+            <div className='loginInput'>
+              <AiOutlineMail />
+              <input type='email' placeholder='e-mail' />
+            </div>
+            <label>* Password </label>
+            <div className='loginInput'>
+              <AiFillLock />
+              <input type='password' placeholder='password' />
+            </div>
+            <Button type='submit' text='Sign In' />
+          </div>
+          <p> ↯ - or connect with - ↯ </p>
+          <div className='anotherLogin'>
+            <Button text={<FcGoogle />} />
+            <Button text={<SiKakaotalk />} />
+          </div>
+        </LoginForm>
+      </LoginPage>
+    </>
   );
 };
+
+const LoginPage = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 500px;
+  height: 600px;
+`;
 
 const LoginForm = styled.form`
   display: flex;
