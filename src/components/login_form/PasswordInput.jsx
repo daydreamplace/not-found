dd01d25e9373718602b4073936d8d6a826586c26
Eden Park <109200582+daydreamplace@users.noreply.github.com>
2022-10-16 01:36:56 +0900

    Add: 회원가입 페이지 추가
    * Refactor: Login Input 재사용 컴포넌트 분리
    
    * Add: 회원가입 페이지 추가
    
    * Modify: 회원가입 창 스타일 수정
    
    * Refactor: button 폴더 이동

---
+++ src/components/login_form/PasswordInput.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { AiFillLock } from 'react-icons/ai';
+import styled from 'styled-components';
+
+const PasswordInput = ({ passwordLabel }) => {
+  return (
+    <Input>
+      <label>* {passwordLabel} </label>
+      <div className='loginInput'>
+        <AiFillLock />
+        <input type='password' placeholder='password' />
+      </div>
+    </Input>
+  );
+};
+
+const Input = styled.div`
+  .loginInput {
+    display: flex;
+    align-items: center;
+    margin-bottom: 10px;
+    padding: 10px;
+
+    border: 1px solid ${({ theme }) => theme.hyperMint};
+    border-radius: 0.625rem;
+
+    &:hover {
+      border: 2px solid ${({ theme }) => theme.hyperMint};
+    }
+
+    input {
+      width: 95%;
+      margin-left: 5px;
+
+      background: transparent;
+      border: none;
+      outline: none;
+    }
+  }
+`;
+
+export default PasswordInput;
