c65a21d68496399f4bbfa77f4d7b641e43e91fb5
youbinpark <you8inpark@gmail.com>
2022-10-16 16:46:58 +0900

    Add: 폰트 추가

--- src/GlobalStyle.jsx
+++ src/GlobalStyle.jsx
@@ -1,4 +1,5 @@
 import { createGlobalStyle } from 'styled-components';
+// import font from './assets/fonts/';
 
 const GlobalStyle = createGlobalStyle`
 
@@ -8,6 +9,11 @@ const GlobalStyle = createGlobalStyle`
   font-weight: 400;
   src: url('//cdn.jsdelivr.net/font-nanumlight/1.0/NanumBarunGothicWeb.eot');
   src: url('//cdn.jsdelivr.net/font-nanumlight/1.0/NanumBarunGothicWeb.eot?#iefix') format('embedded-opentype'), url('//cdn.jsdelivr.net/font-nanumlight/1.0/NanumBarunGothicWeb.woff') format('woff'), url('//cdn.jsdelivr.net/font-nanumlight/1.0/NanumBarunGothicWeb.ttf') format('truetype')}
+
+@font-face {
+  font-family: 'PPRigntSans';
+  src:url('./assets/fonts/PPRightSans.ttf') format("opentype");
+}
 
 body {
     overflow: auto;
@@ -22,6 +28,14 @@ body {
   * {
     box-sizing: border-box;
     background: url('https://hyper-cloud.kr/static/images/background.jpg');
+    cursor: url('https://hyper-cloud.kr/static/images/cursor.png'),auto!important;
+    user-select: auto;
+    scroll-behavior: smooth!important;
+    -webkit-tap-highlight-color: transparent;
+  }
+
+  h1{
+    font-family: "PPRightSans";
   }
 
   a {
